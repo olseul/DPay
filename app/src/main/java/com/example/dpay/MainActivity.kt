6446d0da35f11d35cf27ca33e064f48e9fefd79c
leeseul <seul9085@naver.com>
2022-07-22 15:23:51 +0900

    Feat: 프래그먼트 화면 전환 기능 구현

--- app/src/main/java/com/example/dpay/MainActivity.kt
+++ app/src/main/java/com/example/dpay/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.dpay
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.dpay.chatlist.ChatListFragment
+import com.example.dpay.home.HomeFragment
+import com.example.dpay.mypage.MyPageFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val homeFragment = HomeFragment()
+        val chatListFragment = ChatListFragment()
+        val myPageFragment = MyPageFragment()
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        //초기
+        replaceFragment(homeFragment)
+
+        bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home-> replaceFragment(homeFragment)
+                R.id.chatList -> replaceFragment(chatListFragment)
+                R.id.myPage -> replaceFragment(myPageFragment)
+            }
+            true
+        }
+    }
+
+    //프래그먼트를 관리
+   private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .apply {
+                replace(R.id.fragmentContainer, fragment)
+                commit()
+            }
     }
 }
