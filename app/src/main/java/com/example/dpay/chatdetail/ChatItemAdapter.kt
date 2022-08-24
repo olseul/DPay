4fe00b97b8d6ddbe3a6b8377fb2f5ded6d10b77d
leeseul <seul9085@naver.com>
2022-08-24 17:43:16 +0900

    Feat: 채팅방에서 채팅 시 uid 말고 이름 띄우도록 수정

--- app/src/main/java/com/example/dpay/chatdetail/ChatItemAdapter.kt
+++ app/src/main/java/com/example/dpay/chatdetail/ChatItemAdapter.kt
@@ -6,13 +6,34 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.dpay.databinding.ItemChatBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class ChatItemAdapter : ListAdapter<ChatItem, ChatItemAdapter.ViewHolder>(diffUtil) {
 
+    private lateinit var userDB: DatabaseReference
+
     inner class ViewHolder(private val binding: ItemChatBinding): RecyclerView.ViewHolder(binding.root){
         fun bind(chatItem: ChatItem) {
-            binding.senderTextView.text = chatItem.senderId
-            binding.messageTextView.text = chatItem.message
+
+            userDB = Firebase.database.reference.child("Users")
+            val currentUserDB = userDB.child(chatItem.senderId)
+            currentUserDB.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    binding.senderTextView.text = snapshot.child("name").value.toString()
+                    binding.messageTextView.text = chatItem.message
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+
+                }
+
+            })
+
         }
     }
 
